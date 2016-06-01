39da3713e08160e3fe3bccebf6e7c2fb6385528a
Dan <danielandrewvoss@gmail.com>
2016-06-01 13:05:37 -0400

    Create variables and Contact class

--- app/src/main/java/com/dvoss/contactsandroid/MainActivity.java
+++ app/src/main/java/com/dvoss/contactsandroid/MainActivity.java
@@ -2,12 +2,28 @@ package com.dvoss.contactsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<Contact> contacts;
+
+    ListView list;
+    EditText name;
+    EditText phone;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.listView);
+        name = (EditText) findViewById(R.id.editText);
+        phone = (EditText) findViewById(R.id.editText2);
+        addButton = (Button) findViewById(R.id.button);
     }
 }
