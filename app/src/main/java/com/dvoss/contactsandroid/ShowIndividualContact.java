3706a72bf3dd33de31c29daccba69b499ff1bdd8
Dan <danielandrewvoss@gmail.com>
2016-06-02 09:34:28 -0400

    Add to new activity

--- app/src/main/java/com/dvoss/contactsandroid/ShowIndividualContact.java
+++ app/src/main/java/com/dvoss/contactsandroid/ShowIndividualContact.java
@@ -3,17 +3,24 @@ package com.dvoss.contactsandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowIndividualContact extends AppCompatActivity {
 
-    Intent intent = getIntent();
-    String display = intent.getStringExtra("display");
+    TextView text;
 
-    //Bundle extras = getIntent().getExtras();
+    //  connect to xml
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_individual_contact);
+
+        //text = findViewById(R.id. // textview id?? )
+
+        Intent intent = getIntent();
+        String display = intent.getStringExtra("display");
+
+        // ?? Bundle extras = getIntent().getExtras();
     }
 }
